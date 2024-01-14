b2ede763b02469e4deb9e487118981ccac2dd0ce
lif3ng <lif3ng@icloud.com>
2024-01-14 23:57:45 +0800

    day2: fiber

--- core/react.js
+++ core/react.js
@@ -11,28 +11,93 @@ const createElement = (type, props, ...children) => ({
     ),
   },
 });
-const render = (el, container) => {
-  let dom;
-  if (el.type === "TEXT") {
-    dom = document.createTextNode("");
-    dom.nodeValue = el.props.text;
-  } else {
-    dom = document.createElement(el.type);
-    Object.keys(el.props || {}).forEach((k) => {
-      if (k !== "children") {
-        dom.setAttribute(k, el.props[k]);
-      }
-    });
-    el.props.children?.forEach((el) => {
-      if (typeof el === "string") {
-        el = formatTextNode(el);
-      }
-      render(el, dom);
-    });
+
+let nextUnitOfWork;
+
+const workLoop = (deadline) => {
+  let shouldYield = false;
+  while (!shouldYield && nextUnitOfWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+
+    shouldYield = deadline.timeRemaining() < 1;
   }
 
-  container.append(dom);
+  requestIdleCallback(workLoop);
 };
+
+const createDom = (type) =>
+  type === "TEXT" ? document.createTextNode("") : document.createElement(type);
+
+const initChildren = (fiber) => {
+  const children = fiber.props.children || [];
+  let prevChild = null;
+  children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      props: child.props,
+      child: null,
+      parent: fiber,
+      sibling: null,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevChild.sibling = newFiber;
+    }
+    prevChild = newFiber;
+  });
+};
+
+const updateProps = (dom, props) => {
+  Object.keys(props || {}).forEach((k) => {
+    if (k === "text") {
+      dom.nodeValue = props.text;
+    } else if (k !== "children") {
+      dom.setAttribute?.(k, props[k]);
+    }
+  });
+};
+const performUnitOfWork = (fiber) => {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type));
+    fiber.parent.dom.append(dom);
+    updateProps(dom, fiber.props);
+  }
+
+  initChildren(fiber);
+
+  return fiber.child || fiber.sibling || fiber.parent?.sibling;
+};
+const render = (el, container) => {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [el],
+    },
+  };
+
+  // let dom;
+  // if (el.type === "TEXT") {
+  //   dom = document.createTextNode("");
+  //   dom.nodeValue = el.props.text;
+  // } else {
+  //   dom = document.createElement(el.type);
+
+  //   el.props.children?.forEach((el) => {
+  //     if (typeof el === "string") {
+  //       el = formatTextNode(el);
+  //     }
+  //     render(el, dom);
+  //   });
+  // }
+
+  // container.append(dom);
+};
+
+requestIdleCallback(workLoop);
+
 const React = { createElement, render };
 
 export default React;
